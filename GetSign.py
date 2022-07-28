d54274707686e3f2954deb81d4227fbaca7a8170
Kiryl Pashkin <sleembelbox@gmail.com>
2022-07-28 11:14:30 +0300

    modified 1

---
+++ GetSign.py
@@ -0,0 +1,11 @@
+oper_list = ['-', '+', '*', '/', '=']
+
+def InputSign () -> str:
+    check = False
+    while not check:
+        sign =  input()
+        if sign in oper_list:
+            check = True
+        else:
+            check = False
+    return sign
--- GetValue.py
+++ GetValue.py
@@ -1,3 +1,9 @@
-def 
-
-number = 0
+def InputValues () -> int:
+    check = False
+    while not check:
+        try:
+            number = int(input(f'{text}'))
+            check = True
+        except ValueError:
+            number = int(input(f'{text}'))
+    return number
